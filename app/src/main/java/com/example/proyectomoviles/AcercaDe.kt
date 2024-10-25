b2fac86fbaca0a6f7f575d0c739b0d5b225afe9d
GLa2s <adricarmonagalvez@gmail.com>
2024-10-25 19:41:37 +0200

    el proyecto sin funcionar

--- app/src/main/java/com/example/proyectomoviles/AcercaDe.kt
+++ app/src/main/java/com/example/proyectomoviles/AcercaDe.kt
@@ -1,18 +1,41 @@
 package com.example.proyectomoviles
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.core.provider.FontsContractCompat.Columns
+import java.lang.reflect.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.graphics.Color
+
 
 @Composable
 fun AcercaDe() {
-    Text(
-        text = "AcercaDe"
-    )
-    Image(
-        painter = painterResource(R.drawable.llantaoznegra),
-        contentDescription = "Llanta de coche oz negra"
-    )
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(if (isSystemInDarkTheme())primaryLightMediumContrast else primaryContainerLight),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Text(
+                text = "AcercaDe"
+            )
+            Image(
+                painter = painterResource(R.drawable.llantaoznegra),
+                contentDescription = "Llanta de coche oz negra"
+            )
+        }
+    }
 }
