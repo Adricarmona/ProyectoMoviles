67e12c591d18069d404441a583ae2ed2cd7acdfd
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-11-03 20:51:17 +0100

    el acerca de nosotros y un boton al inicio para ir a esa vista

--- app/src/main/java/com/example/proyectomoviles/AcercaDe.kt
+++ app/src/main/java/com/example/proyectomoviles/AcercaDe.kt
@@ -1,26 +1,27 @@
 package com.example.proyectomoviles
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.core.provider.FontsContractCompat.Columns
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import com.example.proyectomoviles.ui.theme.Pink80
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import com.example.proyectomoviles.ui.theme.ProyectoMovilesTheme
-import com.example.proyectomoviles.ui.theme.Purple80
 import com.example.proyectomoviles.ui.theme.PurpleGrey40
 
 @Composable
@@ -30,28 +31,67 @@ fun AcercaDe() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Pink80),
+            .background(color = MaterialTheme.colorScheme.background),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+
     ) {
         Column {
             Image(
-                painter = painterResource(R.drawable.llantaoznegra),
-                contentDescription = "Llanta de coche oz negra"
+                painter = painterResource(R.drawable.iconoecoring),
+                contentDescription = "icono aplicacion",
+                modifier = Modifier.width(250.dp)
             )
             Spacer(
-                modifier = Modifier.size(0.dp)
+                Modifier.size(20.dp)
             )
+
+            /*
+            Parte de arriba debajo del icono con el titulo y el subtitulo gigantico
+             */
+            EsteticaTitulo(
+                context.getString(R.string.AcercaDe)
+            )
+            EsteticaTitulo(
+                "EcoRing"
+            )
+            TextoNormal(
+                context.getString(R.string.TrabajoDeMoviles)
+            )
+
+            /// ESPACIO ENTRE TITULO Y TEXTO
+            Spacer(
+                Modifier.height(40.dp)
+            )
+            /// ESPACIO ENTRE TITULO Y TEXTO
+
+
+            // el quienes aparecemos en la aplicacion
+            TextoNormal(
+                context.getString(R.string.AplicacionCreada)
+            )
+            TextoNormal(
+                "- Adrian Carmona"
+            )
+            TextoNormal(
+                "- Hector Dominguez"
+            )
+
+            Spacer(
+                Modifier.height(20.dp)
+            )
+
             Text(
-                text = context.getString(R.string.AcercaDe),
-                color = PurpleGrey40,
-                textAlign = TextAlign.Justify
+                text = "Version 0.1",
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center,
             )
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun previewAcercaDe() {
     ProyectoMovilesTheme{
@@ -59,3 +99,28 @@ fun previewAcercaDe() {
     }
 }
 
+@Composable
+fun EsteticaTitulo(text: String)
+{
+    Text(
+        text = text,
+        color = MaterialTheme.colorScheme.primary,
+        textAlign = TextAlign.Center,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.em,
+        letterSpacing = 2.sp,
+        modifier = Modifier.width(250.dp)
+    )
+}
+
+@Composable
+fun TextoNormal(text: String)
+{
+    Text(
+        text = text,
+        color = MaterialTheme.colorScheme.secondary,
+        textAlign = TextAlign.Center,
+        modifier = Modifier.width(250.dp)
+    )
+}
