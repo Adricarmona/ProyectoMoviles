5a2abca8f7baa4908797698b8a2ccd5ab5b073e5
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-11-29 20:04:06 +0100

    fallando cositas pero picando poco a poco

--- app/src/main/java/com/example/proyectomoviles/ui/auth/inicioSesion.kt
+++ app/src/main/java/com/example/proyectomoviles/ui/auth/inicioSesion.kt
@@ -1,27 +1,39 @@
 package com.example.proyectomoviles.ui.auth
 
 import android.content.res.Configuration
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.proyectomoviles.ProyectoMovilesTheme
 import com.example.proyectomoviles.ui.usables.CardDatosLLantas
 
 @Composable
 fun inicioSesion(){
-    val iniciadoSesion by remember MutableState<Boolean>
+    var usuario = remember { mutableStateOf("") }
+    var clave = remember { mutableStateOf("") }
+    var resultado = remember {mutableStateOf("Sin resultado")}
+
+    var context = LocalContext.current
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -36,12 +48,35 @@ fun inicioSesion(){
             color = MaterialTheme.colorScheme.primary
         )
         OutlinedTextField(
-            value = true,
+            value = usuario.value,
             onValueChange = { it ->
-                false = it
-            }
-
+                usuario.value = it
+            },
+            label = {
+                Text(text = "Nombre de usuario")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            singleLine = true
         )
+        OutlinedTextField(
+            value = clave.value,
+            onValueChange = { it ->
+                clave.value = it
+            },
+            label = {
+                Text(text = "Contraseña usuario")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            singleLine = true
+        )
+        Button(
+            onClick = Toast.makeText(context, "aaaa", Toast.LENGTH_SHORT).show();){
+                Text("Loguearse")
+            }
     }
 }
 
