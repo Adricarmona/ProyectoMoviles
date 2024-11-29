e4a2ee14801bdd2918ab72a56ae61a93902180ea
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2024-11-29 12:15:44 +0100

    cambiando nombres y json

--- app/src/main/java/com/example/proyectomoviles/model/cocktail/ClienteCocktailsService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.proyectomoviles.model.cocktail
-
-import retrofit2.http.GET
-
-interface ClienteCocktailsService {
-    @GET("/lookup.php?i=11000")
-    suspend fun getCocktails(): List<CocktailsData>
-}
